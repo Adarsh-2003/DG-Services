34335bdee9eace57bd877e02c3763d749b72daf6
Adarsh Gupta <adgupta183@gmail.com>
2023-12-22 17:32:15 +0530

    resposive nav

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSideBar() {
+    const mobileBar = document.querySelector('.mobile-nav');
+    mobileBar.style.display="block"
+}
+
+function closeSideBar() {
+    const mobileBar = document.querySelector('.mobile-nav');
+    mobileBar.style.display="none"
+}
+
